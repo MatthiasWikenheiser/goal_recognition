ab1f7570c04ffce8a1bdaa752b06cf68f3e252ed
mwiubuntu <matthias.wikenheiser@gmx.de>
2024-07-27 11:45:41 +0200

    not yet implemented: clean circular actions from final plan

--- rl_planner.py
+++ rl_planner.py
@@ -36,12 +36,9 @@ class RlPlanner:
         old_state = self.env.state
         state_collection.append(old_state)
         first_step = True
-
         circular_states = []
-
-
-
         plan_step = 0
+
         while not done:
             print("achieved_goals", [f"achieved_goal_{goal}" for goal in range(1,8)
                                      if self.env.observation_dict[f"achieved_goal_{goal}"]["value"]== 1])
@@ -49,7 +46,7 @@ class RlPlanner:
             time_passed = time.time()-start_time
             if time_passed >= timeout:
                 self.plan = {}
-                print("done here: ", round(time_passed, 2), "s")
+                print("timeout: ", round(time_passed, 2), "s")
                 return
             else:
                 if any([(self.env.state == old).all() for old in state_collection]) and not first_step:
@@ -63,16 +60,19 @@ class RlPlanner:
                     print("calc_time over , ", round(time.time() - calc_start_time, 2), "s")
 
                     if time_passed >= timeout:
-                        print("timeout after possible_actions, ", round(time_passed, 2), "s")
+                        print("timeout: ", round(time_passed, 2), "s")
                         self.plan = {}
                         return
                     if (old_state == self.env.state).all():
                         print("not possible action")
+                        # maintain old q_values and filter for a new possible action
                         q_values_possible_actions = [q_values[0][i] for i in possible_actions]
                         action = possible_actions[np.argmax(q_values_possible_actions)]
                     else:
                         print("in circular state")
-                        # check if state is already listed
+                        self.plan[plan_step] = self.env.action_dict[action]["action_grounded"]
+                        plan_step += 1
+                        # check if circular state is already listed
                         if not any([(self.env.state == circ[0]).all() for circ in circular_states]):
                             circular_states.append([self.env.state, -2])
                             idx = -1
@@ -81,30 +81,30 @@ class RlPlanner:
                             idx = np.argmax([1 if (self.env.state == circ[0]).all() else 0 for circ in circular_states])
                             print("idx: ", idx)
                             circular_states[idx][1] += -1
+                        # state has changed from state before, therefore predict new q-values
                         q_values = self.rl_model.predict(self.env._get_obs_vector()[np.newaxis, :])
                         q_values_possible_actions = [q_values[0][i] for i in possible_actions]
-                        print("circular_states: ", circular_states)
-                        print("circular_states[idx]: ", circular_states[idx])
-                        print("possible_actions: ", possible_actions)
-                        print("action_name: ", [self.env.action_dict[x]["action_grounded"] for x in possible_actions])
-                        print("q_values_possible_actions: ", q_values_possible_actions)
-                        print("np.argsort(q_values_possible_actions): ", np.argsort(q_values_possible_actions))
+                        # print("circular_states: ", circular_states)
+                        # print("circular_states[idx]: ", circular_states[idx])
+                        # print("possible_actions: ", possible_actions)
+                        # print("action_name: ", [self.env.action_dict[x]["action_grounded"] for x in possible_actions])
+                        # print("q_values_possible_actions: ", q_values_possible_actions)
+                        # print("np.argsort(q_values_possible_actions): ", np.argsort(q_values_possible_actions))
                         try:
-                            action = possible_actions[np.argsort(q_values_possible_actions)[circular_states[idx][1]]]
-                            print("np.argsort(q_values_possible_actions)[circular_states[idx][1]]: ",
-                                  np.argsort(q_values_possible_actions)[circular_states[idx][1]])
-                        except:
                             # it might happen that there is only one possible action in this state
                             # therefore, try to find another path in the next state
+                            action = possible_actions[np.argsort(q_values_possible_actions)[circular_states[idx][1]]]
+                            # print("np.argsort(q_values_possible_actions)[circular_states[idx][1]]: ",
+                                  # np.argsort(q_values_possible_actions)[circular_states[idx][1]])
+                        except:
+                            # go back to last idx, since no other possible action is available
                             circular_states[idx][1] += 1
                             action = possible_actions[np.argsort(q_values_possible_actions)[circular_states[idx][1]]]
-                            print("np.argsort(q_values_possible_actions)[circular_states[idx][1]]: ",
-                                  np.argsort(q_values_possible_actions)[circular_states[idx][1]])
+                            # print("np.argsort(q_values_possible_actions)[circular_states[idx][1]]: ",
+                                  # np.argsort(q_values_possible_actions)[circular_states[idx][1]])
 
+                        # print("action: ", (action,self.env.action_dict[action]["action_grounded"] ))
 
-
-                        print("action: ", (action,self.env.action_dict[action]["action_grounded"] ))
-                        self.plan[plan_step] = self.env.action_dict[action]["action_grounded"]
 
                 else:
                     if not first_step:
@@ -117,7 +117,7 @@ class RlPlanner:
                     print(self.env.action_dict[action]["action_grounded"])
                 time_passed = time.time() - start_time
                 if time_passed >= timeout:
-                    print("timeout here, ", round(time_passed, 2), "s")
+                    print("timeout: ", round(time_passed, 2), "s")
                     self.plan = {}
                     return
                 old_state = self.env.state
@@ -126,6 +126,23 @@ class RlPlanner:
 
         # also memorize last action that lead to done
         self.plan[plan_step] = self.env.action_dict[action]["action_grounded"]
+
+        # last step: clean plan
+        if not redundant_actions is None:
+            clean_plan_step = 0
+            clean_plan = {}
+            # remove redundant actions
+            for step in self.plan.keys():
+                if self.plan[step].split("_")[0] not in redundant_actions:
+                    clean_plan[clean_plan_step] = self.plan[step]
+                    clean_plan_step += 1
+            self.plan = clean_plan
+
+        if time_passed >= timeout:
+            print("timeout: ", round(time_passed, 2), "s")
+            self.plan = {}
+            return
+
         print("solved, ",  round(time.time() - start_time, 2), "s")
 
 
@@ -238,6 +255,7 @@ if __name__ == '__main__':
     print("--------")
     print("start_game")
     rl_planner.solve()
+    print(rl_planner.plan)
 
     for sample in samples:
         print("------")
@@ -247,14 +265,3 @@ if __name__ == '__main__':
         print(rl_planner.plan)
 
 
-    #d = []
-    #for key in env.action_dict.keys():
-     #   if env.action_dict[key]["action_ungrounded"] not in d:
-      #      d.append(env.action_dict[key]["action_ungrounded"])
-
-
-    #possible_actions = [a[0] for a in env.get_all_possible_actions(multiprocess=True)
-     #                                       if env.action_dict[a[0]]["action_ungrounded"] not in redundant_actions]
-
-   # q_values = rl_planner.rl_model.predict(rl_planner.env._get_obs_vector()[np.newaxis, :])
-    #q_values_possible_actions = [q_values[0][i] for i in possible_actions]
